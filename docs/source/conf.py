941b5d20c144a705f30f9e21d3a5ffcb1fba5465
Vogt <nick.vogt@tag009442882824.dynatrace.org>
2019-09-11 09:49:05 -0400

    deploy v0.2a

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ copyright = "2019, Dynatrace LLC"
 author = "Nick Vogt"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0dev4"
+release = "0.2.0a"
 
 
 # -- General configuration ---------------------------------------------------
