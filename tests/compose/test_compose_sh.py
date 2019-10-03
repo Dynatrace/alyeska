af744186a3d8ecf5f53620ce83129e9cee51d07c
Ian Heinzman <ian.heinzman@dynatrace.com>
2019-10-02 23:26:28 -0400

    Cleaning up some unused imports (there are a lot) and adding a test for Task file existence.

--- tests/compose/test_compose_sh.py
+++ tests/compose/test_compose_sh.py
@@ -16,16 +16,12 @@
 ## ---------------------------------------------------------------------------
 """Test the compose-sh functionality
 """
-import os
-import pathlib
-
-import pytest
-
-from alyeska.compose.compose_sh import convert_yaml_to_sh
+from alyeska.compose.compose_sh import init_flags, convert_yaml_to_sh
 
 from test_compose_globals import COMPOSE_SMALL, COMPOSE_BIG, COMPOSE_CYCLE
 
 
 def test__convert_yaml_to_sh():
+    init_flags(["foo"])  # Necessary to initialize the flags object.
     actual = convert_yaml_to_sh(COMPOSE_SMALL)
     assert isinstance(actual, str)
